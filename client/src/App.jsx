20d9329eb1e02d7dcbc7632b2c4d8f92e1689686
MichaelLELU <lelu.michael@gmail.com>
2024-04-10 17:30:00 +0200

    feat: imgoftheday.jsx and css responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import ImgOfTheDay from "./components/imgoftheday/ImgOfTheDay";
 
 function App() {
   return (
     <main>
       <h1>SpaceView</h1>
+      <ImgOfTheDay />
     </main>
   );
 }
