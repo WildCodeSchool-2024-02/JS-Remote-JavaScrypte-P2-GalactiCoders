ec3bdbb89e230e72335a9d9a863494be0036939a
Chaymae El Hayani <layani.chay@gmail.com>
2024-04-10 15:37:42 +0200

    feat: add ImgDetail component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import ImgDetails from "./components/imgdetail/Imgdetail";
 
 function App() {
   return (
     <main>
       <h1>SpaceView</h1>
+      <ImgDetails />
     </main>
   );
 }
