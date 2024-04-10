a4d98e10e9ca56fa22e9b88ca43e2678563e2543
Pierre Juppin <pierre.juppin@gmail.com>
2024-04-10 16:52:47 +0200

    feat: add CategoryList whith CSS

---
+++ client/src/components/categorylist/CategoryList.jsx
@@ -0,0 +1,12 @@
+import styles from "./CategoryList.module.css";
+
+
+function CategoryList() {
+return (
+    <div className={styles.categoryDiv}>
+    <h1>Category</h1>
+    </div>
+);
+}
+
+export default CategoryList;
