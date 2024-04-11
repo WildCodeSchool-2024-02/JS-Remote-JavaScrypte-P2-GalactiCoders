318689b5c02fcef732224ac718ec6b7e160f0b9f
Pierre Juppin <pierre.juppin@gmail.com>
2024-04-11 16:06:53 +0200

    feat: add css category

---
+++ client/src/components/category/Category.jsx
@@ -0,0 +1,11 @@
+import styles from "./Category.module.css";
+
+function Category() {
+return (
+    <div className={styles.category}>
+    <h1>Caterory</h1>
+    </div>
+);
+}
+
+export default Category;
