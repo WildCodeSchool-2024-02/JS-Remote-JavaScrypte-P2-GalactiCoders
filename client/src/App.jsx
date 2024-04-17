92fba5e5ac8b3429988306fdb0a23f00ce1ced6d
HippolyteWeber <hippolyte.weber@gmail.com>
2024-04-17 14:11:22 +0200

    fix: app CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import ImgOfTheDay from "./components/imgoftheday/ImgOfTheDay";
 import ImgDetails from "./components/imgdetail/Imgdetail";
 import NavBar from "./components/navbar/NavBar";
 import Category from "./components/category/Category";
+import "./App.css";
 
 function App() {
   const [apiImage, setApiImage] = useState(null);
