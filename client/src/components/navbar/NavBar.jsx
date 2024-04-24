0248bf5430a54b506e26c9a46c3504b3c34dae96
HippolyteWeber <hippolyte.weber@gmail.com>
2024-04-24 09:18:03 +0200

    feat: button home on logo

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,11 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./NavBar.module.css";
 
 function NavBar() {
+  const navigate = useNavigate();
   return (
     <main className={styles.mainContainer}>
       <div className={styles.container}>
         <div className={styles.logoContainer}>
-          <div className={styles.logo} />
+          <div
+            type="button"
+            className={styles.logo}
+            onClick={() => navigate("/")}
+            onKeyDown={() => navigate("/")}
+            role="none"
+          />
+          {/* <Link to="/" onClick={handleOnClick} className={styles.logo}>
+            Home
+          </Link> */}
         </div>
       </div>
     </main>
