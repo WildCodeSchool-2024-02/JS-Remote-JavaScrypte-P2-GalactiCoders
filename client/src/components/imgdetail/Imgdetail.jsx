0284721be651dff74c188e4b23688ad21e659da1
HippolyteWeber <hippolyte.weber@gmail.com>
2024-04-24 10:03:38 +0200

    fix: accept merging

--- client/src/components/imgdetail/Imgdetail.jsx
+++ client/src/components/imgdetail/Imgdetail.jsx
@@ -1,15 +1,33 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 import styles from "./ImgDetail.module.css";
 
 function ImgDetails({ selectedImage }) {
+  
   const description = selectedImage.data[0].description.split(/[.!?]/);
   const firstTwoSentences = description.slice(0, 2).join(".");
   const title = selectedImage.data[0].title.split(/[():.!?]/);
   const firstTitle = title.slice(0, 1).join(":");
 
+  const [closed, setClosed] = useState(false);
+
+
+  useEffect(() => {
+    setClosed(false);
+  }, [selectedImage]);
+
+  const handleClick = () => {
+    setClosed(!closed);
+  };
+
+  if (closed) {
+    return null;
+  }
+
   return (
     <div className={styles.container}>
+      <button className = {styles.closebutton} type="submit" onClick={handleClick}> x </button>
       <img
         className={styles.img}
         src={selectedImage.links[0].href}
@@ -23,6 +41,7 @@ function ImgDetails({ selectedImage }) {
     </div>
   );
 }
+
 ImgDetails.propTypes = {
   selectedImage: PropTypes.string.isRequired,
 };
